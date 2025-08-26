a8eaa9cbb46ccd572bccbb1244372ee2c9a54b3b
Bennett <panzgfx@gmail.com>
2025-08-26 11:53:47 -0400

    logbook work

--- Assets/Scripts/Logbook.cs
+++ Assets/Scripts/Logbook.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,7 +8,46 @@ public class Logbook : MonoBehaviour
     public Image backgroundArt;
     public Sprite[] bgSprites;
     public GameObject[] contents;
+    public RectTransform panel;
+    public bool canOpenClose;
+    public bool isOpen;
+    private PauseMenu pauseMenu;
 
+    private void Start()
+    {
+        pauseMenu = GameController.pauseMenu;
+    }
+
+    private void Update()
+    {
+        if (canOpenClose && isOpen && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Close(); 
+        }
+    }
+    
+    public void Open()
+    {
+        canOpenClose = false;
+        pauseMenu.canOpenClose = false;
+        isOpen = true;
+        panel.gameObject.SetActive(true);
+        panel.DOAnchorPosY(0, .5f).SetEase(Ease.OutBack).SetUpdate(true).OnComplete(()=> canOpenClose = true);
+    }
+    public void Close()
+    {
+        canOpenClose = false;
+        isOpen = false;
+        panel.DOAnchorPosY(-1070, 0.5f).SetEase(Ease.InBack).SetUpdate(true).OnComplete(()=> DoneClose());
+    }
+
+    void DoneClose()
+    {
+        panel.gameObject.SetActive(false);
+        canOpenClose = true;
+        pauseMenu.canOpenClose = true;
+    }
+    
     public void SwitchTab(int index)
     {
         backgroundArt.sprite = bgSprites[index];
