cb4fc40d52086b72a55d003bf6ce1f5c7e7029e3
Bennett <panzgfx@gmail.com>
2025-09-22 13:49:29 -0400

    added slight polish to farmrer select screen

--- Assets/Scripts/Farmer.cs
+++ Assets/Scripts/Farmer.cs
@@ -1,10 +1,12 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Localization;
 using UnityEngine.UI;
 
 public class Farmer : MonoBehaviour
 {
-    public GameObject farmerInfo;
+    public RectTransform farmerInfo;
     public Image farmerImg;
     public int farmerIndex;
     public Sprite unlockedSprite;
@@ -13,6 +15,7 @@ public class Farmer : MonoBehaviour
 
     private void Start()
     {
+        HideFarmerInfo();
         if (isUnlocked)
         {
             farmerImg.sprite = unlockedSprite;
@@ -21,16 +24,16 @@ public class Farmer : MonoBehaviour
 
     public void ShowFarmerInfo()
     {
-        if (farmerInfo.activeInHierarchy)
+        if (farmerInfo.gameObject.activeInHierarchy)
         {
-            farmerInfo.SetActive(false);
+            HideFarmerInfo();
         }
         else
         {
-
             if (isUnlocked)
             {
-                farmerInfo.SetActive(true);
+                farmerInfo.gameObject.SetActive(true);
+                farmerInfo.DOScale(new Vector3(3.571429f,3.571429f,3.571429f), .25f).SetEase(Ease.OutBack);
                 GameController.farmerSelectManager.SelectFarmer(farmerIndex);
             }
             else
@@ -43,6 +46,7 @@ public class Farmer : MonoBehaviour
 
     public void HideFarmerInfo()
     {
-        farmerInfo.SetActive(false);
+        farmerInfo.DOScale(Vector3.zero, .25f).SetEase(Ease.InBack)
+            .OnComplete(() => farmerInfo.gameObject.SetActive(false));
     }
 }
