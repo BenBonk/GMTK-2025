9f5708abe00a8115983804d3d1a5f12668d72716
Bennett <panzgfx@gmail.com>
2025-09-24 13:17:29 -0400

    asdd

--- Assets/Scripts/StartScreenManager.cs
+++ Assets/Scripts/StartScreenManager.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System;
 using System.Collections;
 using UnityEngine;
-using static Unity.VisualScripting.Metadata;
 using DG.Tweening;
 using Random = UnityEngine.Random;
 using TMPro;
