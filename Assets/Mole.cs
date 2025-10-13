e7e533afeba69ea81746b64abd0fab9fa673f1d7
BenBonk <panzgfx@gmail.com>
2025-10-13 10:29:26 -0400

    fixed mole lassoing bug

--- Assets/Mole.cs
+++ Assets/Mole.cs
@@ -9,13 +9,16 @@ public class Mole : MonoBehaviour
     {
         transform.DOScaleY(1, .25f).SetEase(Ease.OutBack);
         yield return new WaitForSeconds(.25f);
+        gameObject.tag = "NonAnimalLassoable";
         collider2D.enabled = true;
         yield return new WaitForSeconds(2);
         if (transform.parent==null)
         {
+            gameObject.tag = "Untagged";
+            collider2D.enabled = false;  
             transform.DOScaleY(0, .25f).SetEase(Ease.InBack);
             yield return new WaitForSeconds(.25f);
-            collider2D.enabled = false;   
+            Destroy(gameObject);
         }
     }
 }
